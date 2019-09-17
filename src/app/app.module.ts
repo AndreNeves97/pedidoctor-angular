e02ecc3aa37ab3070aff67f71bd380272d7eb73a
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 15:09:28 -0300

    Importação do módulo de inputs do angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-
+import { UsuarioModule } from './pedilandia/usuario/usuario.module';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from 'src/environments/environment';
@@ -26,12 +27,13 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
         BrowserModule,
         AppRoutingModule,
 
+        UsuarioModule,
 
         BrowserAnimationsModule,
 
         MainModule,
         
-
+        MatInputModule,
 
         HttpClientModule,
 
