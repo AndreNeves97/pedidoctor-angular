0b26f944f63114e8ac14877316e989111170636a
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 20:42:50 -0300

    Criando modelo de consulta

---
+++ src/app/pedilandia/consulta/consulta.model.ts
@@ -0,0 +1,35 @@
+import { Usuario } from '../usuario/usuario.model';
+
+export class Consulta {
+
+    _id: string;
+    dataConsulta: Date;
+    dataRegistro: Date;
+    paciente: Usuario;
+    tipoConsulta: string;
+    sintomasObservados: string;
+    medicamentosQueToma: string;
+    doencasRecentes: string;
+    informacoesAdicionais: string;
+    
+    constructor (
+        dataConsulta: Date = new Date(),
+        dataRegistro: Date = new Date(),
+        paciente: Usuario = new Usuario(),
+        tipoConsulta: string = "",
+        sintomasObservados: string = "",
+        medicamentosQueToma: string = "",
+        doencasRecentes: string = "",
+        informacoesAdicionais: string = ""
+    ) {
+        this.dataConsulta = dataConsulta;
+        this.dataRegistro = dataRegistro;
+        this.paciente = paciente;
+        this.tipoConsulta = tipoConsulta;
+        this.sintomasObservados = sintomasObservados;
+        this.medicamentosQueToma = medicamentosQueToma;
+        this.doencasRecentes = doencasRecentes;
+        this.informacoesAdicionais = informacoesAdicionais;
+    }
+
+}
