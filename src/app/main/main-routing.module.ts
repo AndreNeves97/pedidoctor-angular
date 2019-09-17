f66d55db09ee2aa7a65e97658291a2c228a6ed36
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 19:55:26 -0300

    Rotas dos componentes de cadastro de usuário e cadastro de consulta

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../main/home/home.component';
+import { CadastroUsuarioComponent } from '../pedilandia/usuario/cadastro-usuario/cadastro-usuario.component';
+import { CadastroConsultaComponent } from './../pedilandia/consulta/cadastro-consulta/cadastro-consulta.component';
 
 const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'pedilandia/usuario', component: CadastroUsuarioComponent},
+    {path: 'pedilandia/consulta', component: CadastroConsultaComponent}
 ]
 
 @NgModule({
