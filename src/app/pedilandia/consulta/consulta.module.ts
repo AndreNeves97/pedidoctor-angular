489fe66b379a453baa744cc98444c1eb0f0a953b
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 20:42:42 -0300

    Adicionando importacoes dos componentes do cadastro de consulta

--- src/app/pedilandia/consulta/consulta.module.ts
+++ src/app/pedilandia/consulta/consulta.module.ts
@@ -1,13 +1,23 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastroConsultaComponent } from './cadastro-consulta/cadastro-consulta.component';
+import { TextMaskModule } from 'angular2-text-mask';
 
 @NgModule({
   declarations: [
     CadastroConsultaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    FormsModule,
+    MatTabsModule,
+    MatButtonModule,
+    TextMaskModule
   ],
   exports: [
     CadastroConsultaComponent
