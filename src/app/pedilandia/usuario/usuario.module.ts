612fba7f7d66eb95eead590dca82987435986a8a
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 15:08:25 -0300

    Criação do módulo de usuários

--- src/app/pedilandia/usuario/usuario.module.ts
+++ src/app/pedilandia/usuario/usuario.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CadastroUsuarioComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ], exports: [
+    CadastroUsuarioComponent
   ]
 })
 export class UsuarioModule { }
