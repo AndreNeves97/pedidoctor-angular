d8d0e77bd2516ba87f3789391b7b3750645ec60b
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 15:33:05 -0300

    Adicionando tabs na página de cadastro

--- src/app/pedilandia/usuario/usuario.module.ts
+++ src/app/pedilandia/usuario/usuario.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     MatInputModule,
     FormsModule,
+    MatTabsModule,
     MatButtonModule,
   ], exports: [
     CadastroUsuarioComponent
