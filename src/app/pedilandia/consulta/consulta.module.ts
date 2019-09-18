976663dfaa32ff1cb7d527b8626c185f4a684ae9
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 21:33:30 -0300

    Adicionando importações de componentes

--- src/app/pedilandia/consulta/consulta.module.ts
+++ src/app/pedilandia/consulta/consulta.module.ts
@@ -4,20 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { CadastroConsultaComponent } from './cadastro-consulta/cadastro-consulta.component';
 import { TextMaskModule } from 'angular2-text-mask';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     CadastroConsultaComponent
   ],
+  providers:[
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+  ],
   imports: [
     CommonModule,
     MatInputModule,
     FormsModule,
+    MatSelectModule,
     MatTabsModule,
     MatButtonModule,
-    TextMaskModule
+    TextMaskModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     CadastroConsultaComponent
