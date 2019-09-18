a5210e97c2fb0395aebc368af38d93df5d7c5b73
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-17 21:33:16 -0300

    Criando enum de tipos de consultas

---
+++ src/app/pedilandia/util/tipo-consulta.enum.ts
@@ -0,0 +1,7 @@
+export enum TipoConsulta {
+    "Diagnóstico" = "DIAGNOSTICO",
+    "Pedido de exame" = "PEDIDO_EXAME",
+    "Apresentação de exame" = "APRESENTACAO_EXAME",
+    "Pós-cirurgia" = "POS_CIRURGIA",
+    "Check-up" = "CHECKUP"
+}
