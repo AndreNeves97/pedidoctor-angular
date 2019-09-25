98c4948c3dd8caaea03c8e6c9aa541b10e602ac7
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-25 20:13:50 -0300

    [?] importando componentes

--- src/app/pedilandia/consulta/consulta.module.ts
+++ src/app/pedilandia/consulta/consulta.module.ts
@@ -5,15 +5,20 @@ import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { CadastroConsultaComponent } from './cadastro-consulta/cadastro-consulta.component';
 import { TextMaskModule } from 'angular2-text-mask';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { ListagemConsultaComponent } from './listagem-consulta/listagem-consulta.component';
 
 @NgModule({
   declarations: [
-    CadastroConsultaComponent
+    CadastroConsultaComponent,
+    ListagemConsultaComponent
   ],
   providers:[
     {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
@@ -24,7 +29,10 @@ import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
     FormsModule,
     MatAutocompleteModule,
     MatSelectModule,
+    MatTableModule,
     MatTabsModule,
+    MatIconModule,
+    MatTooltipModule,
     MatButtonModule,
     TextMaskModule,
     MatDatepickerModule,
