3e80a9219a03ae7a89477c1797f58e2e2621cac5
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-09-24 21:48:37 -0300

    [?] atualizando campo de id no modelo de usuario

--- src/app/common/security/usuario.model.ts
+++ src/app/common/security/usuario.model.ts
@@ -8,11 +8,12 @@ export class Usuario {
     telefone: string;
 
     constructor(params : {
+        _id : string,
         nome: string,
         email: string,
         jwt: string,
         fotoUrl: string,
-        telefone: string,
+        telefone: string
     }) {
         Object.assign(this, params);
     }
