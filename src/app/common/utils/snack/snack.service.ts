44c6ba77b273675c0c7965524eaa33085c700cf1
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-10-22 17:12:11 -0300

    [?] Criado service de snack bar

---
+++ src/app/common/utils/snack/snack.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { SnackComponent } from './snack.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackService {
+
+  constructor(
+    private snack_bar: MatSnackBar
+  ) { }
+
+  /**
+   * Abre uma snack bar de acordo com uma das três gravidades possíveis aceitadas.
+   * 
+   * @param message   por padrão é 'ok'
+   * @param gravidade por padrão é 'success' mas pode ser 'danger' (vermelho) e 'warn' (amarelo)
+   * @param duration  por padrão é 5000
+   */
+  open_snack_bar ( message: string = 'ok', 
+                   gravidade: string = 'success',
+                   duration: number = 5000 ): void {
+
+    this.snack_bar.openFromComponent( SnackComponent, {
+        data : {
+          message,
+          style: gravidade
+        }, 
+        duration
+    });
+
+  }
+
+}
