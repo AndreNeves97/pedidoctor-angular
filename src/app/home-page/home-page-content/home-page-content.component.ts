85100e800ef7f692c20ba02010317ae584326e0b
André Neves <andreneves3@gmail.com>
2019-11-12 00:26:34 -0300

    Página inicial (apresentação do projeto)

---
+++ src/app/home-page/home-page-content/home-page-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-content',
+  templateUrl: './home-page-content.component.html',
+  styleUrls: ['./home-page-content.component.scss']
+})
+export class HomePageContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
