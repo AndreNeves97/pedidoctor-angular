649e916ef57363cc447bdbe981d4a6c36d40d9c4
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 13:47:13 -0300

    Adicionando componente de reportagem de consultas

---
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/realizar-reportagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-realizar-reportagem',
+  templateUrl: './realizar-reportagem.component.html',
+  styleUrls: ['./realizar-reportagem.component.scss']
+})
+export class RealizarReportagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
