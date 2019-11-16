70fe448614f3bbe1e2bb0fd9dae630feb3aa3517
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 17:02:49 -0300

    Estrutura do stepper de receita do diagnóstico do paciente para reportagem de consulta: medicamentos, exames etc

---
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/receita-consulta/receita-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receita-consulta',
+  templateUrl: './receita-consulta.component.html',
+  styleUrls: ['./receita-consulta.component.scss']
+})
+export class ReceitaConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
