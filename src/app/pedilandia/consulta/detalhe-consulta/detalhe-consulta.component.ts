3cac9f9078781c8c423ae1195f11cd40b90e3e15
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 16:04:50 -0300

    Criando componente de detalhes de consultas

--- src/app/pedilandia/consulta/detalhe-consulta/detalhe-consulta.component.ts
+++ src/app/pedilandia/consulta/detalhe-consulta/detalhe-consulta.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ConsultaService } from '../consulta.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Consulta } from '../consulta.model';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-detalhe-consulta',
@@ -7,9 +12,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalheConsultaComponent implements OnInit {
 
-  constructor() { }
+  private consulta: Consulta;
 
-  ngOnInit() {
+  constructor(
+    private service :   ConsultaService,
+    private router  :   Router,
+    private route   :   ActivatedRoute
+  ) { 
+
+    this.route.paramMap
+        .pipe(
+          switchMap((params: ParamMap) => {
+            return of(params.get('id'));
+          })
+        ).subscribe((id: string) => {
+          this.service.find(id).then((consulta: Consulta) =>{
+            if ( consulta )
+              this.consulta = consulta;
+            else
+              this.navigate_back();
+          })
+        });
+
+  }
+
+  ngOnInit() { 
+    this.consulta = new Consulta();
+  }
+
+  private navigate_back() {
+    this.router.navigate(['/pedilandia/consultas']);
+  }
+
+  get dataConsulta () {
+    return this.consulta.dataConsulta;
+  }
+
+  get horario () {
+    return `${this.consulta.dataConsulta.getHours()}:${this.consulta.dataConsulta.getMinutes()}`
+  }
+
+  get paciente () {
+    return this.consulta.paciente;
+  }
+
+  get clinica () {
+    return this.consulta.clinica;
+  }
+
+  get medico () {
+    return this.consulta.medico;
+  }
+
+  get medicamentosQueToma () {
+    if (this.consulta.medicamentosQueToma)
+      return this.consulta.medicamentosQueToma;
+    else 
+      return [];
+  }
+
+  get doencasRecentes () {
+    if (this.consulta.doencasRecentes)
+      return this.consulta.doencasRecentes
+    else  
+      return [];
+  }
+
+  get informacoesAdicionais () { 
+    return this.consulta.informacoesAdicionais;
+  }
+
+  get sintomasObservados () {
+    if (this.consulta.sintomasObservados)
+      return this.consulta.sintomasObservados;
+    else 
+      return [];
+  }
+
+  get tipoConsulta () {
+    return this.consulta.tipoConsulta;
   }
 
 }
