04c274055a30e9a8d1b2a030ad2fd64860b8dce4
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 17:02:14 -0300

    Estrutura do stepper inicial para reportagem de consulta

---
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/detalhes-consulta/detalhes-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-consulta',
+  templateUrl: './detalhes-consulta.component.html',
+  styleUrls: ['./detalhes-consulta.component.scss']
+})
+export class DetalhesConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
