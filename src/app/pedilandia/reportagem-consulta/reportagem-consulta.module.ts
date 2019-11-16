008a178d7938397159b491c8061e1bb14f1dd318
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 17:03:16 -0300

    Importação dos módulos

--- src/app/pedilandia/reportagem-consulta/reportagem-consulta.module.ts
+++ src/app/pedilandia/reportagem-consulta/reportagem-consulta.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RealizarReportagemComponent } from './realizar-reportagem/realizar-reportagem.component';
+import { DetalhesConsultaComponent } from './realizar-reportagem/detalhes-consulta/detalhes-consulta.component';
+import { DiagnosticoConsultaComponent } from './realizar-reportagem/diagnostico-consulta/diagnostico-consulta.component';
+import { ReceitaConsultaComponent } from './realizar-reportagem/receita-consulta/receita-consulta.component';
+import { ConfirmacaoConsultaComponent } from './realizar-reportagem/confirmacao-consulta/confirmacao-consulta.component';
 
 @NgModule({
-  declarations: [RealizarReportagemComponent],
+  declarations: [
+    RealizarReportagemComponent,
+    DetalhesConsultaComponent,
+    DiagnosticoConsultaComponent,
+    ReceitaConsultaComponent,
+    ConfirmacaoConsultaComponent
+  ],
   imports: [
     CommonModule
   ]
