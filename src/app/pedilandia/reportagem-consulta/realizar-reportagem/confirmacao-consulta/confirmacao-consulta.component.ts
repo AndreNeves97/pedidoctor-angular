96eb306dbee9d85ead0e65a917dcea363138208c
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 17:03:07 -0300

    Estrutura do stepper de confirmação da consulta do paciente para reportagem de consulta

---
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/confirmacao-consulta/confirmacao-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmacao-consulta',
+  templateUrl: './confirmacao-consulta.component.html',
+  styleUrls: ['./confirmacao-consulta.component.scss']
+})
+export class ConfirmacaoConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
