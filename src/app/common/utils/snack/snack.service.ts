149a8af650345d4b294401220f23f79c92afc89f
André Neves <andreneves3@gmail.com>
2019-11-16 19:33:47 -0300

    Uso de actions no snackbar

--- src/app/common/utils/snack/snack.service.ts
+++ src/app/common/utils/snack/snack.service.ts
@@ -3,33 +3,43 @@ import { MatSnackBar } from '@angular/material';
 import { SnackComponent } from './snack.component';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SnackService {
 
-  constructor(
-    private snack_bar: MatSnackBar
-  ) { }
+    constructor(
+        private snack_bar: MatSnackBar
+    ) { }
 
-  /**
-   * Abre uma snack bar de acordo com uma das três gravidades possíveis aceitadas.
-   * 
-   * @param message   por padrão é 'ok'
-   * @param gravidade por padrão é 'success' mas pode ser 'danger' (vermelho) e 'warn' (amarelo)
-   * @param duration  por padrão é 5000
-   */
-  open_snack_bar ( message: string = 'ok', 
-                   gravidade: string = 'success',
-                   duration: number = 5000 ): void {
+    /**
+     * Abre uma snack bar de acordo com uma das três gravidades possíveis aceitadas.
+     * 
+     * @param message   por padrão é 'ok'
+     * @param gravidade por padrão é 'success' mas pode ser 'danger' (vermelho) e 'warn' (amarelo)
+     * @param duration  por padrão é 5000
+     */
+    async open_snack_bar(
+        message: string = 'ok',
+        gravidade: string = 'success',
+        duration: number = 5000,
+        action: string = null,
+    ) : Promise<boolean> {
+        const ref = this.snack_bar.open(message, action, {
+            duration: duration
+        });
 
-    this.snack_bar.openFromComponent( SnackComponent, {
-        data : {
-          message,
-          style: gravidade
-        }, 
-        duration
-    });
+        const ret = await ref.afterDismissed().toPromise();
+        return ret.dismissedByAction;
+        
 
-  }
+        // this.snack_bar.openFromComponent(SnackComponent, {
+        //     data: {
+        //         message,
+        //         style: gravidade
+        //     },
+        //     duration
+        // });
+
+    }
 
 }
