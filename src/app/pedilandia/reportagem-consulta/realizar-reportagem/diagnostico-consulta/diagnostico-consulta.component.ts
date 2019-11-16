cb1d7db6fca7cc57b6b75759a9e22dc84c6f5462
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-16 17:02:29 -0300

    Estrutura do stepper de descrição do diagnóstico do paciente para reportagem de consulta

---
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/diagnostico-consulta/diagnostico-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnostico-consulta',
+  templateUrl: './diagnostico-consulta.component.html',
+  styleUrls: ['./diagnostico-consulta.component.scss']
+})
+export class DiagnosticoConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
