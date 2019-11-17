6d9ae943169eed66fb85d3cc3e39640d2521d26d
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-17 16:32:46 -0300

    Removendo referência circular

--- src/app/pedilandia/reportagem-consulta/realizar-reportagem/reportagem-consulta.model.ts
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/reportagem-consulta.model.ts
@@ -3,14 +3,11 @@ import { Diagnostico } from '../../diagnostico/diagnostico.model';
 
 export class ReportagemConsulta {
 
-    consulta: Consulta;
     diagnostico: Diagnostico;
 
     constructor (
-        consulta: Consulta = new Consulta(),
         diagnostico: Diagnostico = new Diagnostico()
     ) {
-        this.consulta = consulta;
         this.diagnostico = diagnostico;
     }
 
