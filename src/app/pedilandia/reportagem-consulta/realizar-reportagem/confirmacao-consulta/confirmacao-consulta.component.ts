dbf316089555979c6739438104784934055654bd
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-17 15:43:12 -0300

    - Recebendo formGroup e consulta via parâmetro do componente

--- src/app/pedilandia/reportagem-consulta/realizar-reportagem/confirmacao-consulta/confirmacao-consulta.component.ts
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/confirmacao-consulta/confirmacao-consulta.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Consulta } from 'src/app/pedilandia/consulta/consulta.model';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-confirmacao-consulta',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmacaoConsultaComponent implements OnInit {
 
+  private form: FormGroup;
+
+  private _consulta: Consulta;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  @Input()
+  set form_group ( form_group: FormGroup ) {
+    this.form = form_group;
+  }
+
+  @Input()
+  set consulta ( consulta: Consulta ) {
+    this._consulta = consulta;
   }
 
 }
--- src/app/pedilandia/reportagem-consulta/realizar-reportagem/receita-consulta/receita-consulta.component.ts
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/receita-consulta/receita-consulta.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Consulta } from 'src/app/pedilandia/consulta/consulta.model';
 
 @Component({
   selector: 'app-receita-consulta',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceitaConsultaComponent implements OnInit {
 
+  private form: FormGroup;
+
+  private _consulta: Consulta;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  @Input()
+  set form_group ( form_group: FormGroup ) {
+    this.form = form_group;
   }
 
+  @Input()
+  set consulta ( consulta: Consulta ) {
+    this._consulta = consulta;
+  }
+  
 }
