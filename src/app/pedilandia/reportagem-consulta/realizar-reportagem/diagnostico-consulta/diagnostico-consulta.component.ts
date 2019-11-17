b3ea7c5521dca59850e63014123da5ea138f749a
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-17 15:15:33 -0300

    Preparando stepper de diagnóstico de consulta

--- src/app/pedilandia/reportagem-consulta/realizar-reportagem/diagnostico-consulta/diagnostico-consulta.component.ts
+++ src/app/pedilandia/reportagem-consulta/realizar-reportagem/diagnostico-consulta/diagnostico-consulta.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Consulta } from 'src/app/pedilandia/consulta/consulta.model';
 
 @Component({
   selector: 'app-diagnostico-consulta',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosticoConsultaComponent implements OnInit {
 
+  private form: FormGroup;
+  private _consulta: Consulta;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  @Input()
+  set form_group ( form: FormGroup ) {
+    this.form = form;
+  }
+
+  @Input()
+  set consulta ( consulta: Consulta ) {
+    this._consulta = consulta;
+    if ( this.form ) {
+      let today = new Date();
+      this.form.patchValue({
+        descricao : 
+`Data: ${ today.getDate() }/${ today.getMonth() }/${ today.getFullYear() }
+Consulta iniciada em: ${ today.getHours() }:${ today.getMinutes() }h
+------------------------------------------------
+Médico: ${ this._consulta.medico.nome }
+Paciente: ${ this._consulta.paciente.nome }
+------------------------------------------------
+Descrição do diagnóstico:`
+      })
+    }
+  }
+
+  get descricao () {
+    return this.form.get('descricao').value;
+  }
+
+  get paciente () {
+    if ( this._consulta )
+      return this._consulta.paciente;
+    return null
   }
 
 }
