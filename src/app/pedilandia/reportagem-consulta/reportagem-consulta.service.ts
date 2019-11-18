a433ca5d97169f1d9cc43268cacfa25428221c5e
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2019-11-18 10:31:40 -0300

    Criando service no padrão bloc para armazenar estado do objeto de consulta durante a realização do agendamento

--- src/app/pedilandia/reportagem-consulta/reportagem-consulta.service.ts
+++ src/app/pedilandia/reportagem-consulta/reportagem-consulta.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Consulta }   from '../consulta/consulta.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportagemConsultaService {
 
+  private source = new BehaviorSubject<Consulta>(new Consulta());
+
   constructor() { }
+
+  public get_consulta ( ) { 
+    return this.source.asObservable();
+  }
+
+  public update( consulta: Consulta ) {
+    this.source.next(consulta);
+  }
+
 }
